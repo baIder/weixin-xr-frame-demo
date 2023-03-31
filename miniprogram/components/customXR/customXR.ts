b5a562730a84e15eddfdfd373686e60eb5847be4
王云阳 <511763880@qq.com>
2023-03-31 13:48:39 +0800

    识别多个图片

--- miniprogram/components/customXR/customXR.ts
+++ miniprogram/components/customXR/customXR.ts
@@ -1,3 +1,6 @@
+import { Scene, XRARTracker } from "XrFrame";
+import { mockData } from "../../mock/data";
+
 // components/customXR/customXR.ts
 Component({
   /**
@@ -9,24 +12,81 @@ Component({
    * 组件的初始数据
    */
   data: {
-    scene: {},
+    scene: {} as Scene,
+    trackerList: {} as { [key: string]: XRARTracker },
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-    handleReady: function ({ detail }: { detail: any }) {
-      this.data.scene = detail.value;
-    },
-    handleTrackerSwitch: function ({ detail }: { detail: any }) {
-      if (detail.value) wx.showToast({ title: `识别成功` });
-    },
-    handleTouchModel: function ({ detail }: { detail: any }) {
-      const { target } = detail.value;
-      const id = target.id;
+    handleReady: async function ({ detail }: { detail: { value: Scene } }) {
+      const scene = detail.value;
+      mockData.forEach(async (i) => {
+        await scene.assets.loadAsset({
+          type: "gltf",
+          assetId: i.id,
+          src: i.model,
+          options: {},
+        });
 
-      this.triggerEvent("onModelTapped", id);
+        const xrFrameSystem = wx.getXrFrameSystem();
+        const shadow = scene.getElementById("shadow-node");
+
+        const arTracker = scene.createElement(xrFrameSystem.XRARTracker, {
+          mode: "Marker",
+          src: i.image,
+        });
+        shadow.addChild(arTracker);
+
+        const tracker = arTracker.getComponent(xrFrameSystem.ARTracker);
+        tracker.el.event.add("ar-tracker-switch", this.handleTrackerSwitch);
+
+        const node = scene.createElement(xrFrameSystem.XRNode);
+        arTracker.addChild(node);
+
+        const gltfElement = scene.createElement(xrFrameSystem.XRGLTF);
+        node.addChild(gltfElement);
+
+        gltfElement
+          .getComponent(xrFrameSystem.Transform)
+          .scale.setArray(i.scale);
+        gltfElement
+          .getComponent(xrFrameSystem.Transform)
+          .position.setArray(i.position);
+        gltfElement
+          .getComponent(xrFrameSystem.Transform)
+          .rotation.setArray(i.rotation);
+
+        const gltfComp = gltfElement.getComponent(xrFrameSystem.GLTF);
+
+        gltfComp.setData({
+          model: scene.assets.getAsset("gltf", i.id),
+        });
+
+        gltfElement.addComponent(xrFrameSystem.CubeShape, {
+          autoFit: true,
+        });
+
+        gltfComp.el.event.add("touch-shape", this.handleTouchModel);
+
+        gltfElement
+          .getComponent(xrFrameSystem.Animator)
+          .setData({ autoPlay: {} });
+      });
+    },
+    handleTrackerSwitch: function () {
+      // handleTrackerSwitch: function ({ detail }: { detail: any }) {
+      wx.showToast({ title: "arTracker" });
+      // if (detail.value) wx.showToast({ title: "识别成功" });
+    },
+    handleTouchModel: function () {
+      // handleTouchModel: function ({ detail }: { detail: any }) {
+      wx.showToast({ title: "点击模型" });
+      // const { target } = detail.value;
+      // const id = target.id;
+
+      // this.triggerEvent("onModelTapped", id);
     },
   },
 });
--- miniprogram/mock/data.ts
+++ miniprogram/mock/data.ts
@@ -11,22 +11,22 @@ export const mockData = [
   },
   {
     id: "xiaoxiongmao",
-    image: "/assets/miku.jpg",
+    image: "https://pic.ntimg.cn/file/20220404/28754952_114328179109_2.jpg",
     model:
       "https://staticfile-cdn.sightp.com/easyar/webar/mini-tracking/xiaoxiongmao.glb",
     scale: [0.4, 0.4, 0.4],
     rotation: [0, 0, 0],
     position: [0, 0, 0],
   },
-  {
-    id: "helmet",
-    image: "/assets/miku.jpg",
-    model:
-      "https://mmbizwxaminiprogram-1258344707.cos.ap-guangzhou.myqcloud.com/xr-frame/demo/damage-helmet/index.glb",
-    scale: [1, 1, 1],
-    rotation: [0, 0, 0],
-    position: [0, 0, 0],
-  },
+  // {
+  //   id: "helmet",
+  //   image: "/assets/miku2.jpg",
+  //   model:
+  //     "https://mmbizwxaminiprogram-1258344707.cos.ap-guangzhou.myqcloud.com/xr-frame/demo/damage-helmet/index.glb",
+  //   scale: [1, 1, 1],
+  //   rotation: [0, 0, 0],
+  //   position: [0, 0, 0],
+  // },
 ];
 
 // module.exports = {
